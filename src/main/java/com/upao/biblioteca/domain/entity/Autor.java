3067216878a46fd945281f0f9384461c2175637f
jhoneiroLove <jhoneiro12@hotmail.com>
2023-12-02 21:04:54 -0500

    javadoc en HUL1

--- src/main/java/com/upao/biblioteca/domain/entity/Autor.java
+++ src/main/java/com/upao/biblioteca/domain/entity/Autor.java
@@ -1,11 +1,19 @@
 package com.upao.biblioteca.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
+
+/**
+ * Representa un autor en el contexto de una biblioteca.
+ * Cada autor tiene un identificador único, nombre, nacionalidad, biografía y fecha de nacimiento.
+ * Además, un autor puede estar asociado con varios libros.
+ *
+ * @author Jhoel Maqui & James Huaman
+ * @version 1.0
+ */
 
 @Table(name = "autores")
 @Entity(name = "Autor")
@@ -15,16 +23,34 @@ import java.util.List;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "autorId")
 public class Autor {
+    /**
+     * Identificador único del autor.
+     */
     @Id
     @Column(name = "autor_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long autorId;
+    /**
+     * Nombre del autor.
+     */
     private String nombre;
+    /**
+     * Nacionalidad del autor.
+     */
     private String nacionalidad;
     @Column(length = 1000)
+    /**
+     * Biografía del autor, con una longitud máxima de 1000 caracteres.
+     */
     private String biografia;
+    /**
+     * Fecha de nacimiento del autor.
+     */
     @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
+    /**
+     * Lista de libros asociados con el autor.
+     */
     @OneToMany(mappedBy = "autor")
     private List<Libro> libro;
 }
--- src/main/java/com/upao/biblioteca/domain/service/AutorService.java
+++ src/main/java/com/upao/biblioteca/domain/service/AutorService.java
@@ -9,19 +9,45 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
+/**
+ * Servicio que ofrece operaciones relacionadas con autores en la biblioteca.
+ *
+ * @author Jhoel Maqui & James Huaman
+ * @version 1.0
+ * @since 1.0
+ */
+
 @Service
 public class AutorService {
     @Autowired
     private final AutorRepository autorRepository;
 
+    /**
+     * Constructor para inyección de dependencias.
+     *
+     * @param autorRepository Repositorio para operaciones CRUD con autores.
+     */
     public AutorService(AutorRepository autorRepository) {
         this.autorRepository = autorRepository;
     }
 
+    /**
+     * Busca un autor por su nombre.
+     *
+     * @param nombre Nombre del autor a buscar.
+     * @return Un Optional que contiene el autor si es encontrado.
+     */
     public Optional<Autor> buscarPorNombre(String nombre) {
         return autorRepository.findByNombre(nombre);
     }
 
+    /**
+     * Guarda o actualiza un autor en la base de datos.
+     *
+     * @param autor El autor a guardar o actualizar.
+     * @return El autor guardado.
+     * @throws ResponseStatusException Si ocurre un error al guardar el autor.
+     */
     public Autor guardarAutor(Autor autor) {
         try {
             return autorRepository.save(autor);
